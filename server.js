b72901d662f59552498d90498b8525da8122d17e
Malith Kalhara <malithkalhara523@gmail.com>
2023-11-18 18:54:25 +0530

    Updated allowed headers

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
     cors({
       origin: 'http://localhost:3000', // Replace with your frontend's URL
       methods: 'GET,POST,OPTIONS',
-      allowedHeaders: 'Content-Type',
+      allowedHeaders: 'Content-Type, Authorization',
       credentials: true,
     })
 );
